7aba373748553613299f239eb5a709dcdc194953
Ignacio Serrano <johndoe@example.com>
2021-11-30 22:16:13 -0300

    Practica 1

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -14,12 +14,15 @@ def imprimir_mayor(numero_1, numero_2):
     # En esta función debe determinar cual de los dos
     # números ingresados por parámetro es mayor
     # y luego imprimir dicho valor en pantalla
-
+    if numero_1 > numero_2:
+        v_max = numero_1
+    else:
+        v_max = numero_2
+    return(v_max)
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    
     # Alumno: Complete la función "imprimir_mayor"
-    imprimir_mayor(2, 10)
-
+    v_max =  imprimir_mayor(2, 10)
+    print(f'El valor mayor de los dos es {v_max}')
     print("terminamos")
